a89ce6107114c788be38c50eae4e3539943c1767
Bauti Garaventa <bautistagaraventa@hotmail.com>
2025-06-09 18:19:13 -0300

    created 3 necessary endpoints properly

--- src/localization_management_api/main.py
+++ src/localization_management_api/main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
+from localization_management_api.supabase.queries import fetch_localizations, fetch_all_localizations, get_translation_completion
 
 app = FastAPI()
 
-## This is the endpoint to get the localizations for a project and locale
-## It returns a JSON object with the localizations for the project and locale
+@app.get("/localizations/{locale}")
+async def get_all_localizations(locale: str):
+    localizations = await fetch_all_localizations(locale)
+    return {
+        "locale": locale,
+        "localizations": localizations
+    }
+
 @app.get("/localizations/{project_id}/{locale}")
 async def get_localizations(project_id: str, locale: str):
-    return {"project_id": project_id, "locale": locale, "localizations": {"greeting": "Hello", "farewell": "Goodbye"}}
+    localizations = await fetch_localizations(project_id, locale)
+    return {
+        "project_id": project_id,
+        "locale": locale,
+        "localizations": localizations
+    }
+
+@app.get("/analytics/completion/{project_id}")
+async def get_completion(project_id: int):
+    data = await get_translation_completion(project_id)
+    return data
